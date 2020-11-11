1aed1c34c86e566a7125633fa6d4926bf9957f4b
cristianagreda <cagreda@ubits.co>
2020-11-11 18:43:22 -0500

    First Commit

--- src/main/java/com/ubitslearning/customersuccessapi/Hello.java
+++ src/main/java/com/ubitslearning/customersuccessapi/Hello.java
@@ -1,14 +1,19 @@
 package com.ubitslearning.customersuccessapi;
 
+import com.ubitslearning.customersuccessapi.nodel.Request;
+import com.ubitslearning.customersuccessapi.nodel.Response;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
 
 import java.util.function.Function;
 
 @Component
-public class Hello implements Function<String, String> {
+public class Hello implements Function<Request, Response> {
 
     @Override
-    public String apply(String s) {
-        return "hello class"+ s;
+    public Response apply(Request s) {
+
+        Response response = Response.builder().response("hello class" + s.getName()).build();
+        return response;
     }
 }
---
+++ src/main/java/com/ubitslearning/customersuccessapi/nodel/Request.java
@@ -0,0 +1,13 @@
+package com.ubitslearning.customersuccessapi.nodel;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Request {
+
+    private String name;
+}
