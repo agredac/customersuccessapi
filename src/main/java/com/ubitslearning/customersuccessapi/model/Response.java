18fd127b86de2d6a80f7b83326765c8d24429477
cristianagreda <cagreda@ubits.co>
2020-11-12 10:30:47 -0500

    Models

--- src/main/java/com/ubitslearning/customersuccessapi/model/Response.java
+++ src/main/java/com/ubitslearning/customersuccessapi/model/Response.java
@@ -1,10 +1,14 @@
 package com.ubitslearning.customersuccessapi.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Response {
 
     private String result;
